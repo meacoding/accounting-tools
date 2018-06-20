1be0f28965677fe39383c504228503bab1e9d83f
Amanda Harness <16675876+meacoding@users.noreply.github.com>
2018-06-20 08:53:15 -0400

    Worked on partialUnits

--- src/renderer/components/partialUnits/partialUnits.vue
+++ src/renderer/components/partialUnits/partialUnits.vue
@@ -1,49 +1,46 @@
 <template>
   <div id="partialUnits">Partial Units
-    <!-- <div class="card">
-      <div class="card-body"> -->
-        <div class="row">
-          <div class="col-6">
+    <div class="row">
+      <div class="col-6">
 
-              <div class="input-group input-group-sm mb-3">
-                  <div class="input-group-prepend">
-                      <span class="input-group-text inputWidth">Invoice Unit Price</span>
-                  </div>
-                  <input type="number"
-                          @focus="$event.target.select()"
-                          class="form-control"
-                          placeholder="Invoice Unit Price"
-                          aria-label="Invoice Unit Price"
-                          aria-describedby="basic-addon1"
-                          v-model="invoiceUP">
-
+          <div class="input-group input-group-sm mb-3">
+              <div class="input-group-prepend">
+                  <span class="input-group-text inputWidth">Invoice Unit Price</span>
               </div>
-              <div class="input-group input-group-sm mb-3">
-                  <div class="input-group-prepend">
-                      <span class="input-group-text inputWidth">PO Unit Price</span>
-                  </div>
-                  <input type="number"
-                          @focus="$event.target.select()"
-                          class="form-control"
-                          placeholder="PO Unit Price"
-                          aria-label="Purchase Order Unit Price"
-                          aria-describedby="basic-addon1"
-                          v-model="poUP">
+              <input type="number"
+                      @focus="$event.target.select()"
+                      class="form-control"
+                      placeholder="Invoice Unit Price"
+                      aria-label="Invoice Unit Price"
+                      aria-describedby="basic-addon1"
+                      v-model="invoiceUnitPrice">
 
+          </div>
+          <div class="input-group input-group-sm mb-3">
+              <div class="input-group-prepend">
+                  <span class="input-group-text inputWidth">PO Unit Price</span>
               </div>
-          </div>
+              <input type="number"
+                      @focus="$event.target.select()"
+                      class="form-control"
+                      placeholder="PO Unit Price"
+                      aria-label="Purchase Order Unit Price"
+                      aria-describedby="basic-addon1"
+                      v-model="poUnitPrice">
 
-          <div class="col-6">
-              Total Tax: <strong>{{numberWithCommasAndDollarSign(totalTax)}}</strong>
-              <br>
-              Sub Total: <strong>{{numberWithCommasAndDollarSign(subTotal)}}</strong>
-              <br>
-              Total:  <strong>{{numberWithCommasAndDollarSign(total)}}</strong>
           </div>
-        </div>
       </div>
-    <!-- </div>
-  </div> -->
+
+      <div class="col-6">
+          Partial unit decimal to receive: <span>{{ partialUnitDecimal }}</span>
+          <br>
+          Temporary unit price for PO: <span>{{ tempUnitPriceForPO }}</span>
+          <br>
+          <!-- Per unit extension in Spectrum:  <strong>{{numberWithCommasAndDollarSign(total)}}</strong> -->
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -55,12 +52,43 @@
         return '$' + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
       }
     },
+    data () {
+      return {
+        invoiceUnitPrice: 0,
+        poUnitPrice: 0,
+        storedPartialUnitDecimal: 0,
+        storedTempUnitPriceForPO: 0
+      }
+    },
     computed: {
-      total () {
+      partialUnitDecimal: {
+        get () {
+          if (!this.invoiceUnitPrice || !this.poUnitPrice) return
+          let number = (Number(this.invoiceUnitPrice) / Number(this.poUnitPrice)).toFixed(2)
+          console.log('partialUnitDecimal', number)
+          console.log('invoiceUnitPrice', this.invoiceUnitPrice)
+          console.log('poUnitPrice', this.poUnitPrice)
+          return this.numberWithCommasAndDollarSign(number)
+        },
+        set (value) {
+          console.log('partialUnitDecimal2', value)
+          this.storedPartialUnitDecimal = value
+          console.log('storedPartialUnitDecimal', this.storedPartialUnitDecimal)
+        }
       },
-      subTotal () {
-      },
-      totalTax () {
+      tempUnitPriceForPO: {
+        get () {
+          if (!this.invoiceUnitPrice || !this.poUnitPrice) return
+          let number2 = (Number(this.invoiceUnitPrice) / Number(this.storedPartialUnitDecimal).toFixed(2))
+          console.log('tempUnitPriceForPO', number2)
+          console.log('storedPartialUnitDecimal', this.storedPartialUnitDecimal)
+          return this.numberWithCommasAndDollarSign(number2)
+        },
+        set (value) {
+          console.log('tempUnitPriceForPO', value)
+          this.partialUnits.storedTempUnitPriceForPO = value
+          console.log('storedTempUnitPriceForPO', this.storedTempUnitPriceForPO)
+        }
       }
     }
   }
